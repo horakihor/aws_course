5e6732046da374b9fb472ef122a07befc5e0febf
Ihor Horak <horakihor00@gmail.com>
2019-05-31 13:03:04 +0300

    Format logs in Json

--- app/web/main/__init__.py
+++ app/web/main/__init__.py
@@ -1,8 +1,9 @@
 import logging
 from logging.handlers import RotatingFileHandler
+from pythonjsonlogger import jsonlogger
 
 log = logging.getLogger('werkzeug')
 file_handler = RotatingFileHandler('main.log', 'a', 1 * 1024 * 1024, 10)
-file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+file_handler.setFormatter(jsonlogger.JsonFormatter('%(asctime) %(levelname) %(module) %(funcName) %(lineno) %(message)'))
 log.setLevel(logging.DEBUG)
 log.addHandler(file_handler)
